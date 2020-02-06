993ab1a4ba71e935de9bcff5fa22f552137b31d0
IvanSergeev1 <57568287+IvanSergeev1@users.noreply.github.com>
2020-02-06 09:27:13 +0300

    Update main.js

--- 2.2-object-prototype/main.js
+++ 2.2-object-prototype/main.js
@@ -1,3 +1,4 @@
+"use strict";
 function initCheckBirthday() {
     const birthday = document.getElementById('birthday').value;
 
@@ -7,7 +8,11 @@ function initCheckBirthday() {
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
+    // код для задачи №1 писать здесь
+    let now = Date.now();
+    birthday = Number(new Date(birthday));
+    let diff = (now - birthday) / 31556736000;
+    return (diff > 18);
 }
 
 function initPrintAnimalSound() {
@@ -21,7 +26,13 @@ function initPrintAnimalSound() {
 }
 
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
+    // код для задачи №2 писать здесь
+  if (animal == 'undefined') {
+		return null;
+  }
+  else {
+  	return animal.sound;	
+  }
 }
 
 function initCalculateStatement() {
@@ -35,5 +46,13 @@ function initCalculateStatement() {
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
+    // код для задачи №3 писать здесь
+
+    let roundedAverage = 0;
+    for (mark of marks) {
+    	roundedAverage += Number(mark);
+    }
+console.log(roundedAverage);
+    roundedAverage = Math.round(roundedAverage / marks.length);
+    return roundedAverage;
 }
